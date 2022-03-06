592c8e0f40c75f5ea47a3af7fbc73b8fec8f0c02
GitHub <abduqahhor2349@gmail.com>
2022-03-06 14:01:17 +0500

    Initial Commit

---
+++ FlexibilityDemo/FlexibilityDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  FlexibilityDemoApp.swift
+//  FlexibilityDemo
+//
+//  Created by Abduqaxxor on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FlexibilityDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
